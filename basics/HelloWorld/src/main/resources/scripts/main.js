5221eeca9a447e780d8eff861359163aa660e18f
Ramin <RaminShamsi@yahoo.com>
2017-07-24 22:24:24 +0200

    add the changing photo onclick

--- basics/HelloWorld/src/main/resources/scripts/main.js
+++ basics/HelloWorld/src/main/resources/scripts/main.js
@@ -4,11 +4,22 @@
 
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!';
-if(myHeading.textContent === 'Hello World!'){
+if (myHeading.textContent === 'Hello World!') {
   alert('Yeah, that is hello world');
 } else {
   alert('Nooo, come on!');
 }
+
 document.querySelector('html').onclick = function () {
   alert('Ouch! Stop poking me!')
+}
+
+var myImage = document.querySelector('img');
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/1.jpg') {
+    myImage.setAttribute('src', 'images/2.jpg');
+  } else {
+    myImage.setAttribute('src', 'images/1.jpg');
+  }
 }
